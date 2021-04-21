c801395fb81a67e5a49a987adb1700716f21b12b
John Vehmeyer <j.vehmeyer@gmail.com>
2021-04-21 14:26:34 -0500

    completed task 3

--- index.js
+++ index.js
@@ -74,10 +74,16 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
   }
   
+  // Car.prototype.fill() = function(gallons){
+  //   return gallons + `${this.tank}`;
+  // } 
   
   /*
     TASK 3
@@ -86,10 +92,21 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+    this.name = name;
+    this.age = age;
+    this.favoriteToy = favoriteToy;
   }
+
+  Baby.prototype = Object.create(Person.prototype);
+
+  Baby.prototype.play = function(){
+    return `Playing with ${this.favoriteToy}`;
+  }
+
+
  
+
   
   /* 
     TASK 4
